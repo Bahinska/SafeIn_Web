275d938316240631f10773d9110e957bcfda00c9
Marharyta Bahinska <marharytabahinska@gmail.com>
2023-01-27 15:22:10 +0200

    Project start

---
+++ SafeIn_web/SafeIn_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SafeIn_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
