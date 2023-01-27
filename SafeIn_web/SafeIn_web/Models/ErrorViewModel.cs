37d2fe2e524737192d61e8cb86c34648dc9e5211
Margo <97248807+Bahinska@users.noreply.github.com>
2023-01-27 22:02:23 +0200

    Delete SafeIn_web directory

--- SafeIn_web/SafeIn_web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SafeIn_web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
